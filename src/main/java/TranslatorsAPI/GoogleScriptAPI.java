3d7d25916879bfe22ae22a0219ea1358f0070a00
stepanstefan <steve111112@yandex.ru>
2021-05-18 02:21:24 +0300

    added base reaization of google script API

---
+++ src/main/java/TranslatorsAPI/GoogleScriptAPI.java
@@ -0,0 +1,33 @@
+package TranslatorsAPI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class GoogleScriptAPI implements Translator {
+    @Override
+    public String translate(String langFrom, String langTo, String text) throws IOException {
+        String urlStr = "https://script.google.com/macros/s/AKfycbyr8KmE_zJCBSUFTQzHSbmvUOnhjrE2yQyBlUuA27-n9YdB_SsU_NLLRw5sImlPmk65/exec" +
+                "?q=" + URLEncoder.encode(text, StandardCharsets.UTF_8) +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            if (inputLine.contains("&#39;")) {
+                inputLine = inputLine.replace("&#39;", "'");
+            }
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+}
