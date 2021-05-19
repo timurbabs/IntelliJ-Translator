a8a3d215728655c5ba7de60b550a3965c042f10b
stepanstefan <steve111112@yandex.ru>
2021-05-20 01:42:46 +0300

    refactor TranslatorForm, GoogleScriptAPI and InputReplaceSelectedText

---
+++ src/main/java/actions/InputReplaceSelectedText.java
@@ -0,0 +1,51 @@
+package actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import forms.TranslatorForm;
+import org.jetbrains.annotations.NotNull;
+import translatorsAPI.GoogleScriptAPI;
+import translatorsAPI.Languages;
+
+import java.io.IOException;
+
+public class InputReplaceSelectedText extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        final Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
+        final String selectedText = editor.getSelectionModel().getSelectedText();
+        final Project project = event.getRequiredData(CommonDataKeys.PROJECT);
+        final Document document = editor.getDocument();
+        final Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
+        if (selectedText != null) {
+            TranslatorForm.showDialogWithTwoInputs().ifPresent(stringStringSimpleEntry ->
+                    WriteCommandAction.runWriteCommandAction(project, () -> {
+                        try {
+                            document.replaceString(
+                                    primaryCaret.getSelectionStart(), primaryCaret.getSelectionEnd(),
+                                    new GoogleScriptAPI().translate(
+                                            Languages.valueOf(stringStringSimpleEntry.getKey()).getTitle(),
+                                            Languages.valueOf(stringStringSimpleEntry.getValue()).getTitle(),
+                                            selectedText
+                                    )
+                            );
+                        } catch (IOException e) {
+                            Messages.showMessageDialog("Failed to translate.",
+                                    TranslatorForm.TITLE, Messages.getErrorIcon());
+                        }
+                    }));
+            primaryCaret.removeSelection();
+        } else {
+            Messages.showMessageDialog("Please select a text for translation.",
+                    TranslatorForm.TITLE, Messages.getWarningIcon());
+        }
+    }
+}
--- src/main/java/translatorsAPI/GoogleScriptAPI.java
+++ src/main/java/translatorsAPI/GoogleScriptAPI.java
@@ -11,15 +11,15 @@ import java.nio.charset.StandardCharsets;
 public class GoogleScriptAPI implements Translator {
     @Override
     public String translate(String langFrom, String langTo, String text) throws IOException {
-        String urlStr = "https://script.google.com/macros/s/AKfycbyr8KmE_zJCBSUFTQzHSbmvUOnhjrE2yQyBlUuA27-n9YdB_SsU_NLLRw5sImlPmk65/exec" +
+        final String urlStr = "https://script.google.com/macros/s/AKfycbyr8KmE_zJCBSUFTQzHSbmvUOnhjrE2yQyBlUuA27-n9YdB_SsU_NLLRw5sImlPmk65/exec" +
                 "?q=" + URLEncoder.encode(text, StandardCharsets.UTF_8) +
                 "&target=" + langTo +
                 "&source=" + langFrom;
-        URL url = new URL(urlStr);
-        StringBuilder response = new StringBuilder();
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        final URL url = new URL(urlStr);
+        final StringBuilder response = new StringBuilder();
+        final HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
         while ((inputLine = in.readLine()) != null) {
             if (inputLine.contains("&#39;")) {
@@ -28,6 +28,7 @@ public class GoogleScriptAPI implements Translator {
             response.append(inputLine);
         }
         in.close();
+
         return response.toString();
     }
 }
