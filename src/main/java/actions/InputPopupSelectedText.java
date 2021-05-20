74588ad6636e446623c9436914287f335bd5a426
stepanstefan <steve111112@yandex.ru>
2021-05-20 22:47:07 +0300

    added input-popUp action

---
+++ src/main/java/actions/InputPopupSelectedText.java
@@ -0,0 +1,52 @@
+package actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.ui.JBColor;
+import com.intellij.ui.awt.RelativePoint;
+import forms.TranslatorForm;
+import org.jetbrains.annotations.NotNull;
+import translatorsAPI.GoogleScriptAPI;
+import translatorsAPI.Languages;
+
+import java.io.IOException;
+
+public class InputPopupSelectedText extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        final Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
+        final String selectedText = editor.getSelectionModel().getSelectedText();
+        final Project project = event.getRequiredData(CommonDataKeys.PROJECT);
+        if (selectedText != null) {
+            TranslatorForm.showDialogWithTwoInputs().ifPresent(stringStringSimpleEntry ->
+                    WriteCommandAction.runWriteCommandAction(project, () -> {
+                        try {
+                            final String result = new GoogleScriptAPI().translate(
+                                    Languages.valueOf(stringStringSimpleEntry.getKey()).getTitle(),
+                                    Languages.valueOf(stringStringSimpleEntry.getValue()).getTitle(),
+                                    selectedText
+                            );
+                            final RelativePoint relPoint = JBPopupFactory.getInstance()
+                                    .guessBestPopupLocation(editor.getSelectionModel().getEditor());
+                            JBPopupFactory.getInstance().createHtmlTextBalloonBuilder(
+                                    result, null, JBColor.LIGHT_GRAY, null
+                            )
+                                    .createBalloon()
+                                    .show(relPoint, Balloon.Position.below);
+                        } catch (IOException e) {
+                            TranslatorForm.showTranslateErrorMessage();
+                        }
+                    }));
+        } else {
+            TranslatorForm.showNoSelectedMessage();
+        }
+    }
+}
+
