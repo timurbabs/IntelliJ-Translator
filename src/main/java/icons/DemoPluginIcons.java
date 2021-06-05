2edf1b92a944871138fdebc25cf46e0d7161aefd
irybtsov <rybtsov_i_v@mail.ru>
2021-06-05 03:27:19 +0300

    add: added right-click menu and icon

---
+++ src/main/java/icons/DemoPluginIcons.java
@@ -0,0 +1,9 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface DemoPluginIcons {
+    Icon DemoAction = IconLoader.getIcon("./icon.svg", DemoPluginIcons.class);
+}
