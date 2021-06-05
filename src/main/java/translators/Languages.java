7d1647dfe9385943f972db1107abd67e39db563b
Borisov Stepan <steve111112@yandex.ru>
2021-06-05 12:25:33 +0300

    Minor changes

--- src/main/java/translators/Languages.java
+++ src/main/java/translators/Languages.java
@@ -11,8 +11,33 @@ public enum Languages {
     Chinese ("zh"),
     Czech ("cs"),
     Italian ("it"),
-    Japanese ("ja");
-
+    Japanese ("ja"),
+    Arabic ("ar"),
+    Afrikaans ("af"),
+    Bengali ("bn"),
+    Bulgarian ("bg"),
+    Danish ("da"),
+    Finnish ("fi"),
+    Georgian ("ka"),
+    Greek ("el"),
+    Hindi ("hi"),
+    Indonesian ("id"),
+    Irish ("ga"),
+    Kazakh ("kk"),
+    Latin ("la"),
+    Macedonian ("mk"),
+    Mongolian ("mn"),
+    Norwegian ("no"),
+    Polish ("pl"),
+    Portuguese ("pt"),
+    Romanian ("ro"),
+    Serbian ("sr"),
+    Slovak ("sk"),
+    Swedish ("sv"),
+    Thai ("th"),
+    Turkish ("tr"),
+    Vietnamese ("vi");
+    
     private final String title;
 
     Languages(String title) {
